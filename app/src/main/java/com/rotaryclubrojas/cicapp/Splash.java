1c3828d9101949fb9a93425f1393cdfbdf1cb8f1
juani <juanma.svazquez@gmail.com>
2016-11-03 20:07:26 -0300

    Cambios en imagenes Splash

--- app/src/main/java/com/rotaryclubrojas/cicapp/Splash.java
+++ app/src/main/java/com/rotaryclubrojas/cicapp/Splash.java
@@ -2,52 +2,43 @@ package com.rotaryclubrojas.cicapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.widget.ProgressBar;
 
+import com.zl.reik.dilatingdotsprogressbar.DilatingDotsProgressBar;
+
 public class Splash extends Activity {
-    public static final int segundos=3;
-    public static final int milisegundos=segundos*1000;
-    public static final int delay=2;
-    private ProgressBar progreso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        progreso= (ProgressBar) findViewById(R.id.pbProgreso);
-        progreso.setMax(maximo_progreso());
-        animacion();
+        DilatingDotsProgressBar progressBar = (DilatingDotsProgressBar)findViewById(R.id.progress);
+        progressBar.showNow();
+        new InitLoader().execute();
     }
 
-    public void animacion (){
-        new CountDownTimer(milisegundos,1000){
+    class InitLoader extends AsyncTask<String, String, String>{
 
-            @Override
-            public void onTick(long millisUntilFinished) {
-                progreso.setProgress(establecer_progreso(millisUntilFinished));
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
 
-            @Override
-            public void onFinish() {
-                Intent nuevofrom=new Intent(Splash.this,Horarios.class);
-                startActivity(nuevofrom);
-                finish();
-            }
-        }.start();
-    }
+            return null;
+        }
 
-    public int establecer_progreso(long miliseconds){
-
-        return (int) ((milisegundos-miliseconds)/1000);
-
-    }
-
-    public int maximo_progreso (){
-
-        return segundos-delay;
-
+        @Override
+        protected void onPostExecute(String s) {
+            Intent nuevofrom=new Intent(Splash.this,Horarios.class);
+            startActivity(nuevofrom);
+            finish();
+        }
     }
 }
